6044a7b0e9e5e8ce526ea12bfdde3b7fa9f10a34
Elena Litvinova <alenalitvinava@gmail.com>
2017-10-20 16:29:05 +0300

    Proper webpack configs

---
+++ config/shared.blocks.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const { group, entryPoint } = require('@webpack-blocks/webpack2');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ROOT_PATH = path.resolve(__dirname, '..');
+const DIST_PATH = path.resolve(ROOT_PATH, 'dist');
+
+const entrypoint = () => entryPoint('./src/index.js');
+
+const output = () => () => ({
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(DIST_PATH),
+    },
+});
+
+const babelLoader = () => () => ({
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                    },
+                }
+            },
+        ]
+    }
+});
+
+const createHtml = () => () => ({
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './src/index.template.html',
+        }),
+    ],
+});
+
+const basicConfig = () => (
+    group([
+        entrypoint(),
+        output(),
+        babelLoader(),
+        createHtml(),
+    ])
+);
+
+module.exports = {
+    basicConfig,
+}
---
+++ config/webpack.production.config.js
@@ -0,0 +1,44 @@
+const { basicConfig } = require('./shared.blocks.js');
+const { createConfig } = require('@webpack-blocks/webpack2');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const prodScssToCss = () => () => ({
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            minimize: true,
+                        },
+                    }, {
+                        loader: 'sass-loader',
+                    }, {
+                        loader: 'resolve-url-loader',
+                    }],
+                }),
+            },
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].bundle.css',
+        }),
+    ],
+});
+
+const uglifyJs = () => () => ({
+    plugins: [
+        new UglifyJSPlugin(),
+    ],
+});
+
+module.exports = createConfig([
+    basicConfig(),
+    prodScssToCss(),
+    uglifyJs(),
+]);
